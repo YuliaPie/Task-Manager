528a1cbc39793ebb178952086008ebc7e79573ad
Yulia <jilly-2005@yandex.ru>
2024-08-13 20:55:58 +0300

    message added explictly for successfull user update

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import ImproperlyConfigured
 from django.db.models import ProtectedError
 from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
 from django.views.generic import View, UpdateView, CreateView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import CustomUser
 from .forms import UserForm, UserUpdateForm
-from django.urls import reverse_lazy
-from django.contrib import messages
 from task_manager.tools import check_and_redirect_if_not_auth
 import logging
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 logger = logging.getLogger(__name__)
 
@@ -33,12 +34,33 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     success_message = "Пользователь успешно зарегистрирован."
 
 
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class UserUpdateView(UserPassesTestMixin, UpdateView, SuccessMessageMixin):
     model = CustomUser
     form_class = UserUpdateForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('users:users')
     success_message = "Пользователь успешно изменен."
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(
+                request,
+                "Вы не авторизованы! "
+                "Пожалуйста, выполните вход.", extra_tags='danger')
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
+
+    def test_func(self):
+        if self.request.user.pk != self.get_object().pk:
+            return False
+        return True
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request,
+            "У вас нет прав для изменения другого пользователя.",
+            extra_tags='danger')
+        return HttpResponseRedirect(reverse_lazy('users:users'))
 
 
 def user_confirm_delete(request, user_id):
